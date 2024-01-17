6016aa3087cd88f4165fc3729f6ecf51c7bf8944
Davivii <137809935+Davivii@users.noreply.github.com>
2024-01-17 08:50:43 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,20 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace atividade_3
+namespace atividade_4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine();
-            Console.ReadLine();
-            double pi = 3.14;
-            int piQuebrado = (int)pi;
-            MessageBox.Show("piQuebrado = " + piQuebrado);
+            Console.ReadLine()
+            int a = 1;
+            int b = -3;
+            int c = -4;
+           
+            double delta = b * b - 4 * a * c;
+            double a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+           
+            MessageBox.Show("a1 = " + a1 + "\na2 = " + a2);
             Console.WriteLine();
-            Console.ReadLine();
-
+            Console.ReadLine()
 
         }
     }
