4d152b1480086a80d50354df80f8b1d0fd0062b0
Davivii <137809935+Davivii@users.noreply.github.com>
2024-01-17 08:30:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine();
+            Console.ReadLine();
+            int idadejose = 10;
+            int idadetasha = 25;
+            int idadevanessa = 30;
+            // Calcule a média das idades.
+            double mediaIdades = (idadejose + idadetasha + idadevanessa) / 3.0;
+            // Exiba o resultado em um MessageBox.
+            MessageBox.Show("A média das idades é: " + mediaIdades);
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
